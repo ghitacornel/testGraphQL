452e83949eda830f5ecc63cd7f75b21b7645e36b
cornel <ghita.cornel@gmail.com>
2022-05-03 12:37:04 +0300

    better

--- src/test/java/demo/VehiclesTest.java
+++ src/test/java/demo/VehiclesTest.java
@@ -1,24 +1,41 @@
 package demo;
 
-import com.graphql.spring.boot.test.GraphQLResponse;
-import com.graphql.spring.boot.test.GraphQLTest;
-import com.graphql.spring.boot.test.GraphQLTestTemplate;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import demo.repositories.entities.Vehicle;
+import graphql.kickstart.spring.webclient.boot.GraphQLRequest;
+import graphql.kickstart.spring.webclient.boot.GraphQLResponse;
+import graphql.kickstart.spring.webclient.boot.GraphQLWebClient;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@GraphQLTest
+//@SpringBootTest(
+//        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+//        properties = {
+//                "server.port=8080"
+//        })
 public class VehiclesTest {
 
-    @Autowired
-    private GraphQLTestTemplate graphQLTestTemplate;
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     public void findAll() throws IOException {
-        GraphQLResponse response = graphQLTestTemplate.postForResource("graphql/findAll.graphql");
-        assertThat(response.isOk()).isTrue();
+        WebClient webClient = WebClient.builder()
+                .baseUrl("http://localhost:" + "8080" + "/graphql")
+                .build();
+
+        GraphQLWebClient graphQLWebClient = GraphQLWebClient.newInstance(webClient, objectMapper);
+
+        GraphQLResponse entity = graphQLWebClient.post(GraphQLRequest.builder()
+                        .query(Files.readString(Paths.get("src", "test", "resources", "graphql", "findById.graphql").toAbsolutePath()))
+                        .variables(Map.of("id", "1")).build())
+                .block();
+        Assertions.assertThat(entity.getFirst(Vehicle.class)).isNull();
     }
 }
